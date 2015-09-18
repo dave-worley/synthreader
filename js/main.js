b924f8b53a7f154f6408add97be4406863e6dac7
David Worley <davidworley@gmail.com>
2015-09-18 11:34:24 -0700

    Pause function. Also the whole thing works

--- js/main.js
+++ js/main.js
@@ -1,28 +1,97 @@
-(function ($, game) {
-    var timedReader = function () {
-        var gamewindow, word, letters, coloredWord;
-        gamewindow = $('#gamewindow');
-        word = game.wordlist[Math.floor(Math.random() * game.wordlist.length)];
+(function ($, _, game) {
+    var PAUSED = false;
+    var createTag = function (tag) {
+        return $('<' + tag + '></' + tag + '>');
+    };
+    var colorWord = function (word, tag) {
+        var letters, letter, cword;
+        cword = createTag(tag);
         letters = word.split('');
-        coloredWord = $('<h1></h1>');
         for (var i = 0; i < letters.length; i++) {
-            var span, letter;
-            span = $('<span></span>');
-            letter = letters[i];
-            span.addClass(letter);
-            span.text(letter);
-            coloredWord.append(span);
+            letter = letters[i];            
+            cword.append(createTag('span').addClass(letter.toLowerCase()).text(letter));
         }
-        gamewindow.html(coloredWord);
+        return cword;
+    };
+    var getNewWord = function () {
+        var words = _.filter(window.game.wordlist, function (n) {
+            return n.length == window.game.wordLengthLimit;
+        });
+        var position = Math.floor(Math.random() * words.length);
+        return colorWord(words[position], 'h1')
+    };
+    var showNext = function () {
+        $('#gamewindow').html(getNewWord());
+    };
+    var timedReader = function () {
+        showNext();
     };
     timedReader();
-    var interval = 3;
-    var loop = setInterval(timedReader, interval * 1000);
-    $('#interval_form').submit(function () {
+
+    var createAnimationLoop = function (f, ival) {
+        return setInterval(f, ival * 1000);
+    };
+    var loop = createAnimationLoop(timedReader, 1);
+
+    // set up the interval control
+    $("#interface > span").each(function () {
+        var value = parseInt($(this).text(), 10);
+        $(this).empty().slider({
+            value: value,
+            min: 1,
+            max: 10,
+            step: 1,
+            range: "min",
+            animate: true,
+            orientation: "vertical"
+        });
+    });
+
+    $("#interval-control").slider("option", "min", 0.25);
+    $("#interval-control").slider("option", "max", 20);
+    $("#interval-control").slider("option", "step", 0.25);
+    $("#interval-control").on("slidechange", function (event, ui) {
+        if (!PAUSED) {
+            clearInterval(loop);
+            loop = createAnimationLoop(timedReader, ui.value);
+            $('#interval-display').text("interval: " + ui.value);
+        }
+    });
+
+    $("#length-control").slider("option", "max", 8);
+    $("#length-control").on("slidechange", function (event, ui) {
+        window.game.wordLengthLimit = ui.value;
+        $('#length-display').text("word length: " + ui.value);
+
+        if (!PAUSED) {
+            clearInterval(loop);
+            loop = createAnimationLoop(timedReader, $("#interval-control").slider("value"));
+        }
+
+    });
+
+    $("#interface > button#pause").click(function () {
+        PAUSED = true;
+        clearInterval(loop);
+        $(this).hide();
+        $("button#next").show();
+    });
+    $("#interface > button#next").click(function () {
+        showNext();
+    });
+
+    $(document).on('keydown', function (event) {
+        if (event.which === 39 && PAUSED) {
+            showNext();
+            event.preventDefault();
+        }
+    });
+
+
+/*    $('#interval_form').submit(function () {
         interval = parseFloat($('#interval').val());
-        console.log(interval);
         clearInterval(loop);
         loop = setInterval(timedReader, interval * 1000);
         return false;
-    });
-})(jQuery, game);
+    });*/
+})(jQuery, _, game);
